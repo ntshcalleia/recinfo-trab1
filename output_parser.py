d8f00831c94549abecede469fa19d7dd148773cd
Natasha Rocha <nattycdb@gmail.com>
2019-05-07 11:48:39 -0300

    Conserta número das consultas
    Consultas vão de 1 a 10, mas tinha colocado 0 a 9.

--- output_parser.py
+++ output_parser.py
@@ -2,15 +2,15 @@ import json
 import os
 from types import SimpleNamespace
 
-path = './output/'
+path = './output2/'
 
-outfile = open("output.txt","w+")
+outfile = open("output2.txt","w+")
 
 for filename in os.listdir(path):
-    with open(path+filename, "r", encoding="mbcs") as read_file:
+    with open(path+filename, "r") as read_file:
         data = json.load(read_file, object_hook=lambda d: SimpleNamespace(**d))
 
     for document in data.hits:
-        outfile.write(filename[0] + ' Q0 ' + (vars(document.doc)['docno'])[0] + ' ' + str(data.hits.index(document)) + ' ' + str(document.score) + ' natasha_guilherme' + '\n')
+        outfile.write(str(int(filename[0])+1) + ' Q0 ' + (vars(document.doc)['docno'])[0] + ' ' + str(data.hits.index(document)) + ' ' + str(document.score) + ' natasha_guilherme' + '\n')
 
 outfile.close()
